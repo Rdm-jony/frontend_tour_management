99d6c426971ca8ad676973b791eb89e376f2ec4d
Rdm-jony <jonydascse@gmail.com>
2025-10-27 00:45:26 +0600

    set navbar

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button"
 import Hero from "@/components/modules/Home/Hero"
 
 export default function Home() {
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -22,7 +22,7 @@ const navigationLinks = [
 
 export default function Navbar() {
   return (
-    <header className="border-b px-4 md:px-6">
+    <header className="border-b px-4 md:px-6 sticky top-0 z-30 bg-white">
       <div className="flex h-16 items-center justify-between gap-4">
         {/* Left side */}
         <div className="flex items-center gap-2">
