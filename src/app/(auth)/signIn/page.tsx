4d397a913c9db6373fc6585a2b2d42b373ae5710
Rdm-jony <jonydascse@gmail.com>
2025-10-31 18:35:01 +0600

    set up login and middleware

---
+++ src/app/(auth)/signIn/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from '@/components/modules/Auth/LoginForm';
+
+const SignIn = () => {
+    return (
+        <div>
+            <LoginForm />
+        </div>
+    );
+};
+
+export default SignIn;
