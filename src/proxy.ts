d9aea9756307e71706abae6cf42c127114bf770e
Rdm-jony <jonydascse@gmail.com>
2025-10-31 19:39:29 +0600

    set signUp page

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,16 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function proxy(request:NextRequest) {
-    const {pathname}=request.nextUrl
-    const token=request.cookies.get("accessToken")?.value
-    const authRoutes=["/signIn"]
-    const isAuthRoute=authRoutes.some(route=>route==pathname)
-    if(isAuthRoute && token){
-        return NextResponse.redirect(new URL("/",request.url))
+export function proxy(request: NextRequest) {
+    const { pathname } = request.nextUrl
+    const token = request.cookies.get("accessToken")?.value
+    const authRoutes = ["/signIn", "/signUp"]
+    const isAuthRoute = authRoutes.some(route => route == pathname)
+    if (isAuthRoute && token) {
+        return NextResponse.redirect(new URL("/", request.url))
     }
 
     return NextResponse.next()
 }
 export const config = {
-  matcher: ['/signIn'],
+    matcher: ['/signIn', "/signUp"],
 }
