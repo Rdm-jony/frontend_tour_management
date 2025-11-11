9645aa34042c3208c2d35d1a1a4b2ebe7e7f4cae
Rdm-jony <jonydascse@gmail.com>
2025-11-11 23:10:48 +0600

    refactor code

--- src/app/(commonLayout)/tours/page.tsx
+++ src/app/(commonLayout)/tours/page.tsx
@@ -1,6 +1,5 @@
 import ToursClient from "@/components/modules/Tours/ToursClient";
 import { getAllTour } from "@/utils/tour";
-import { Suspense } from "react";
 
 
 export default async function ToursPage() {
@@ -9,9 +8,7 @@ export default async function ToursPage() {
   return (
     <div className="p-5">
       <h1 className="text-2xl font-semibold mb-6">All Tours</h1>
-      <Suspense fallback={<>loading....</>}>
-        <ToursClient initialData={data} />
-      </Suspense>
+      <ToursClient initialData={data} />
     </div>
   );
 }
